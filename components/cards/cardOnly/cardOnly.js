409d3dc1b870a128ca28a1a00b1ae55577dc0334
Leisy Sánchez <leisysado@gmail.com>
2026-04-13 16:55:01 -0500

    Add cards and styles from components folder in views

--- components/cards/cardOnly/cardOnly.js
+++ components/cards/cardOnly/cardOnly.js
@@ -1,10 +1,19 @@
-export function cardOnly(title, description) {
+export function cardOnly(title, description, size = "md") {
+  const sizes = {
+    xs: "12rem",
+    sm: "18rem",
+    md: "26rem",
+    lg: "36rem",
+    xl: "48rem",
+    xxl: "60rem",
+  };
+
   return `
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${description}</p>
-            </div>
+    <div class="card card-custom shadow" style="width: ${sizes[size] || sizes.md};">
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
         </div>
-    `;
+    </div>
+  `;
 }
--- views/about/app.js
+++ views/about/app.js
@@ -1,5 +1,11 @@
 import { cardOnly } from "../../components/cards/cardOnly/cardOnly.js";
 
-const containerDescription = document.querySelector("container-group");
+const containerDescription = document.querySelector(".container-group");
 
-containerDescription.appendChild(cardOnly("Grupo dinamita", "Leisy"));
+containerDescription.innerHTML = cardOnly(
+    "Vértice Club",
+  `Somos un equipo de desarrolladores enfocados en diseñar y desarrollar un aplicativo web para un gimnasio, orientado a la gestión de reservas. Buscamos crear una plataforma intuitiva que permita a los usuarios consultar horarios, reservar sesiones y administrar sus actividades de forma sencilla.
+
+Nuestro objetivo es optimizar la organización del gimnasio y mejorar la experiencia de los clientes mediante una solución eficiente, moderna y fácil de usar.`,
+"xl"
+);
