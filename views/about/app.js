fabdba38078e36de1505ba3dfe6964e0517db9c0
Leisy Sánchez <leisysado@gmail.com>
2026-04-13 14:57:14 -0500

    add cardOnly to containerGroup

--- views/about/app.js
+++ views/about/app.js
@@ -1,1 +1,5 @@
+import { cardOnly } from "../../components/cards/cardOnly/cardOnly.js";
+
 const containerDescription = document.querySelector("container-group");
+
+containerDescription.appendChild(cardOnly("Grupo dinamita", "Leisy"));
